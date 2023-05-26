3f555136c6b984fb28eb99db213da7afb9a503d8
Eduardo Franco <franc0edu24@gmail.com>
2023-05-26 20:08:55 -0300

    update by eduardo

--- aula1/Program.cs
+++ aula1/Program.cs
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-﻿//Alteração realizada por Gabriel Rodrigues
+//Alteração realizada por Eduardo Franco
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-=======
-﻿// Código feito por Gustavo Benedito
+// Código feito por Gustavo Benedito
 Console.WriteLine("Hello, World!");
->>>>>>> GustavoBenedito
