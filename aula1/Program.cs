b592683adc8903badaad2d5a9a602c7010fc56ad
Eduardo Franco <franc0edu24@gmail.com>
2023-05-26 17:27:45 -0300

    feat: print na tela do Eduardo

--- aula1/Program.cs
+++ aula1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Linha alterada pelo Eduardo
+Console.WriteLine("Eduardo alterou essa linha");
