6cdc7fa6a6ae8ee737279fb4a230960ccb70d4c5
Gabriel Rodrigues <gabers357@gmail.com>
2023-05-27 11:34:07 -0300

    criada estrutura do codigo

--- aula1/Program.cs
+++
@@ -1,6 +0,0 @@
-//Alteração realizada por eduardo
-//Alteração realizada por Eduardo F.
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Código feito por Gustavo Benedito
-Console.WriteLine("Hello, World!");
